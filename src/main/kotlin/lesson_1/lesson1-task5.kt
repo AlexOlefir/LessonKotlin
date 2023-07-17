a99d3b2cab3ff3340333a28bf6e3315b57ec4205
ProgectK <olefir78@mail.ru>
2023-07-17 17:09:57 +0300

    LK-1-5 Lesson 1 Task 5 time in space Yuri Gagarin edit

--- src/main/kotlin/lesson_1/lesson1-task5.kt
+++ src/main/kotlin/lesson_1/lesson1-task5.kt
@@ -1,13 +1,23 @@
 package lesson_1
 
-fun main(){
+import java.text.Format
+import kotlin.time.Duration.Companion.hours
 
-    val seconds : Short =  6480
+
+fun main() {
+    val seconds = 6480
     val remainderSeconds = seconds % 60
-    val minute  = seconds / 60
+    val minute = seconds / 60
     val remainderMinute = minute % 60
     val hour = minute / 60
-
-    println("0$hour:$remainderMinute:0$remainderSeconds")
-
+    var proverkaHour: String = "0"
+    if (hour < 10) {
+        proverkaHour = proverkaHour + hour.toString()
+    }
+    var proverkaSecond: String = "0"
+    if (remainderSeconds < 10) {
+        proverkaSecond = proverkaSecond + remainderSeconds.toString()
+    }
+    println("$proverkaHour:$remainderMinute:$proverkaSecond")
 }
+
