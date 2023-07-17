317224d135afe149ca19a064545215e6984d8752
ProgectK <olefir78@mail.ru>
2023-07-17 14:45:39 +0300

    LK-1-3 Lesson 1 Task 3 the flight of Yuri Gagarin edit

--- src/main/kotlin/lesson_1/lesson1-task3.kt
+++ src/main/kotlin/lesson_1/lesson1-task3.kt
@@ -5,17 +5,17 @@ fun main() {
     // Переменная года полета Юрия Гагарина
     val year: String = "1961"
     // Переменная час взлета
-    var hour = 9
+    var hour:String = "09"
     // Переменная минута взлета
-    var minute = 7
+    var minute:String = "07"
 
     println("Год полета $year")
     println("Час взлета $hour")
     println("Минута взлета $minute")
 
     // меняем значения переменных
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     // Вывод на печать
     print("Время посадки $hour:$minute")
