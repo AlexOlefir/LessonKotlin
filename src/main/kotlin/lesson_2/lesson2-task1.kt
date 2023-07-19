1924eff274017301359b4e022dc34ab888a155a9
ProgectK <olefir78@mail.ru>
2023-07-19 16:50:05 +0300

    LK-2-1 Lesson 2 Task 1 Average score calculation Исправил имя переменной, перенес на 10 строку ее иницилизацию

--- src/main/kotlin/lesson_2/lesson2-task1.kt
+++ src/main/kotlin/lesson_2/lesson2-task1.kt
@@ -3,10 +3,9 @@ package lesson_2
 fun main() {
     val student = listOf<Float>(3f, 4f, 3f, 5f)
     var sum = 0f
-    var ItogSum: Float
     for (n in 0..student.size - 1) {
         sum += student[n]
     }
-    ItogSum = sum / student.size.toInt()
-    println("Средний балл по школе:$ItogSum")
+    var itogSum = sum / student.size.toInt()
+    println("Средний балл по школе:$itogSum")
 }
