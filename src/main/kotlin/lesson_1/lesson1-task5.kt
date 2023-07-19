bd6b802f032cd5ce9646c56c3d7866787c19c6db
ProgectK <olefir78@mail.ru>
2023-07-19 16:43:38 +0300

    LK-1-5 Lesson 1 Task 5 time in space Yuri Gagarin edit Сделал через string format, лишнее убрал

--- src/main/kotlin/lesson_1/lesson1-task5.kt
+++ src/main/kotlin/lesson_1/lesson1-task5.kt
@@ -1,23 +1,12 @@
 package lesson_1
 
-import java.text.Format
-import kotlin.time.Duration.Companion.hours
-
 
 fun main() {
     val seconds = 6480
-    val remainderSeconds = seconds % 60
+    val rSeconds = seconds % 60
     val minute = seconds / 60
-    val remainderMinute = minute % 60
+    val rMinute = minute % 60
     val hour = minute / 60
-    var proverkaHour: String = "0"
-    if (hour < 10) {
-        proverkaHour = proverkaHour + hour.toString()
-    }
-    var proverkaSecond: String = "0"
-    if (remainderSeconds < 10) {
-        proverkaSecond = proverkaSecond + remainderSeconds.toString()
-    }
-    println("$proverkaHour:$remainderMinute:$proverkaSecond")
+    println(String.format("%02d:%02d:%02d", hour, rMinute, rSeconds))
 }
 
