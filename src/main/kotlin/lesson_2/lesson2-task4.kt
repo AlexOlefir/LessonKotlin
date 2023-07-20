2b3f107a3f4d60ee5e61e2141664d560b07aa1e3
ProgectK <olefir78@mail.ru>
2023-07-20 09:25:48 +0300

    LK-2-4 Lesson 2 Task 4 Bonus Ore Сделал бафф через явное указание процентов

--- src/main/kotlin/lesson_2/lesson2-task4.kt
+++ src/main/kotlin/lesson_2/lesson2-task4.kt
@@ -1,13 +1,12 @@
 package lesson_2
 
 fun main() {
-    val crystalOre = 7
-    val ironOre = 11
-    val baff = 1.2
-    val crystalOrebaff = crystalOre * baff
-    val ironOrebaff = ironOre * baff
-    val crystalbaff = (crystalOrebaff - crystalOre).toInt()
-    val ironlbaff = (ironOrebaff - ironOre).toInt()
-    println("Количество бонусной кристаллической руды: $crystalbaff")
-    println("Количество бонусной железной руды: $ironlbaff")
+    val crystalOre = 7.0
+    val ironOre = 11.0
+    val baff = 20
+    val crystalOrebaff = ((crystalOre / 100) * baff).toInt()
+    val ironOrebaff = ((ironOre / 100) * baff).toInt()
+
+    println("Количество бонусной кристаллической руды: $crystalOrebaff")
+    println("Количество бонусной железной руды: $ironOrebaff")
 }
