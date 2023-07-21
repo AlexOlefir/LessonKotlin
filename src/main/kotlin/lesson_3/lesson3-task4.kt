43e6fc8df2d0e4f83266908a7905d521f6d1b989
ProgectK <olefir78@mail.ru>
2023-07-21 13:53:10 +0300

    LK-3-4 Lesson 3 Task 4 Chess

---
+++ src/main/kotlin/lesson_3/lesson3-task4.kt
@@ -0,0 +1,21 @@
+package lesson_3
+
+fun main() {
+    var cells = "E" // Буква ячейки, на которой стояла фигура
+    var numberCells = 2 //Номер ячейки, на которой стояла фигура
+    var cellsEdit = "E" // Буква ячейки, на которую пошла фигура
+    var numberCellsEdit = 4 //Номер ячейки, на которую пошла фигура
+    var motionNumber = 1 //Номер хода
+    var motion: String
+    motion = cells + numberCells + cellsEdit + numberCellsEdit + motionNumber
+
+    println(motion)
+
+    cells = "D"
+    numberCells = 2
+    cellsEdit = "D"
+    numberCellsEdit = 3
+    motionNumber = motionNumber + 1
+    motion = cells + numberCells + cellsEdit + numberCellsEdit + motionNumber
+    println(motion)
+}
